c2d1d7811fc23d3668b82ba9e44e588c09143bf6
Rafiul29 <rafiulislam4472@gmail.com>
2024-07-12 12:28:35 +0600

    product sow home page

--- stylehaven/urls.py
+++ stylehaven/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('', home,name='home' ),
 ]
 
-urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
